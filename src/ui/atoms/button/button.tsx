3d15ac690c59f396fa334c4d4d2bab6f9223a59d
Johan E. Sierra Linares <johanse.linares@gmail.com>
2022-12-29 20:04:41 -0400

    New button component

---
+++ src/ui/atoms/button/button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button, ButtonProps as ButtonPropsUI } from "theme-ui";
+
+export type ButtonProps = ButtonPropsUI;
+
+const ButtonWrapper = (props: ButtonProps) => {
+  return <Button {...props} />;
+};
+
+export default ButtonWrapper;
--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -1,5 +1,9 @@
 export { default as Icon } from "./icon";
 export { default as MenuLink } from "./menuLink";
 export { default as Skeleton } from "./skeleton";
-export { default as ButtonLink } from "./buttonLink";
+
 export { default as Link } from "./link";
+export type { LinkProps } from "./link";
+
+export { default as Button } from "./button";
+export type { ButtonProps } from "./button";
